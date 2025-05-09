d50b31cafd582f7a0010f234a83e0839d06a53f5
Várday Gábor <91201672+szegabor@users.noreply.github.com>
2025-05-09 19:18:15 +0200

    Update main.py

--- project_root/main.py
+++ project_root/main.py
@@ -1,6 +1,6 @@
 from image_pipeline import ImageProcessingPipeline
 
 if __name__ == "__main__":
-    folder_path = "c:\\Users\\varda\\Documents\\szakdolgozat\\opencv app\\proba kod\\dataset\\skittles"
+    folder_path = "c:\\Users\\varda\\Documents\\skittles"
     pipeline = ImageProcessingPipeline(folder_path)
     pipeline.run()
